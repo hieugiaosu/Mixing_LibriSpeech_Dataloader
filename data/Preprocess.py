9f1f4fe5986cfac8e98e2763e5a9229627ae3777
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-06-04 19:26:41 +0700

    add Time frame feature block

--- data/Preprocess.py
+++ data/Preprocess.py
@@ -1,4 +1,6 @@
-import torch 
+import torch
+import torchaudio.transforms as T
+import torch.nn.functional as F
 from data.DataException import SilenceWindow
 
 # def SilenceCheck(level = 0.01,percentage = 0.5):
@@ -8,15 +10,17 @@ from data.DataException import SilenceWindow
 #             raise SilenceWindow
 #         return silence
 #     return proc
-
-def mix2Speakers(audio:torch.Tensor,paddingMap:torch.Tensor,label:torch.Tensor):
-    invAudio = torch.flip(audio,[0])
-    invlabel = torch.flip(label,[0])
-    audioAct = 1 - paddingMap
-    invAudioAct = torch.flip(audioAct,[0])
-    diff = (invlabel != label).float()
-    invAudioAct = invAudioAct*diff.unsqueeze(1)
-    sumTerm = audioAct+invAudioAct 
-    sumTerm = torch.max(sumTerm,torch.tensor(1.0))
-    mix = audio*(audioAct/sumTerm) + invAudio*(invAudioAct/sumTerm)
-    return mix
+def Mix2Speakers():
+    spec = T.Spectrogram(512,hop_length=160)
+    def mixAudio(audio:torch.Tensor,paddingMap:torch.Tensor,label:torch.Tensor):
+        invAudio = torch.flip(audio,[0])
+        invlabel = torch.flip(label,[0])
+        audioAct = 1 - paddingMap
+        invAudioAct = torch.flip(audioAct,[0])
+        diff = (invlabel != label).float()
+        invAudioAct = invAudioAct*diff.unsqueeze(1)
+        sumTerm = audioAct+invAudioAct 
+        sumTerm = torch.max(sumTerm,torch.tensor(1.0))
+        mix = audio*(audioAct/sumTerm) + invAudio*(invAudioAct/sumTerm)
+        return mix, F.normalize(spec(mix))
+    return mixAudio
