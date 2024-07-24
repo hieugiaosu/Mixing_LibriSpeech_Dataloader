cad005b1efe15995ab929b10814b0d1953da9ca7
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-07-24 22:28:56 +0700

    change the filter in model

---
+++ I Wanna Hear Your Voice/network/modules/gate_module.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch
+
+class BandFilterGate(nn.Module):
+    def __init__(self,emb_dim=48, n_freqs = 65):
+        super().__init__()
+        self.alpha = nn.Parameter(torch.empty(1,emb_dim,n_freqs,1).to(torch.float32))
+        self.beta = nn.Parameter(torch.empty(1,emb_dim,n_freqs,1).to(torch.float32))
+        nn.init.ones_(self.alpha)
+        nn.init.ones_(self.beta)
+    def forward(self,input,filters,bias):
+        f = self.alpha*filters
+        b = self.beta*bias
+        return f*input + b
+        
