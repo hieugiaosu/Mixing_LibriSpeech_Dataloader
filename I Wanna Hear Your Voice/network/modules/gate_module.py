952fc1bb92ad3cc98f3c3a24b4021dd3283b5bc0
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-07-25 12:13:46 +0700

    change init weight in gate

--- I Wanna Hear Your Voice/network/modules/gate_module.py
+++ I Wanna Hear Your Voice/network/modules/gate_module.py
@@ -6,8 +6,8 @@ class BandFilterGate(nn.Module):
         super().__init__()
         self.alpha = nn.Parameter(torch.empty(1,emb_dim,n_freqs,1).to(torch.float32))
         self.beta = nn.Parameter(torch.empty(1,emb_dim,n_freqs,1).to(torch.float32))
-        nn.init.ones_(self.alpha)
-        nn.init.ones_(self.beta)
+        nn.init.xavier_normal_(self.alpha)
+        nn.init.xavier_normal_(self.beta)
     def forward(self,input,filters,bias):
         f = self.alpha*filters
         b = self.beta*bias
