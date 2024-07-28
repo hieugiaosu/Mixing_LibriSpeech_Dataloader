e8bbe24a67a5b88e31c16592e585313492bc3b40
Tanio253 <thanh.tran159@hcmut.edu.vn>
2024-07-28 18:45:56 +0700

    add LOTH module

--- I_Wanna_Hear_Your_Voice/example.py
+++ I_Wanna_Hear_Your_Voice/example.py
@@ -1,4 +1,4 @@
-from network.models import FilterBandTFGridnet
+from network.models import FilterBandTFGridnet, TargetSpeakerLOTH
 from data import getTrainAndValSetFromMetadata, KAGGLE_ROOT
 from training.FilterBandTFGridnetPipeline import FilterBandTFPipeline
 
@@ -8,7 +8,31 @@ train_ds, val_ds = getTrainAndValSetFromMetadata(
     test_size = 0.1                                            
     )
 
-model = FilterBandTFGridnet(n_layers=5)
+model = TargetSpeakerLOTH(n_layers=5)
+
+# pipe = FilterBandTFPipeline(
+#     model = model, 
+#     train_dataset = train_ds, 
+#     val_dataset = val_ds, 
+#     optimizer="AdamW", 
+#     optimizer_param={
+#         "lr":1e-3,
+#         "weight_decay":1.0e-2
+#     }, 
+#     train_batch_size=8, 
+#     val_batch_size=8, 
+#     epochs=200, 
+#     time_limit=3600*12 - 360, 
+#     device='cuda', 
+#     using_multi_gpu=True, 
+#     checkpoint_path="./", 
+#     checkpoint_name="FilterBandTFGridnet.pth", 
+#     checkpoint_rate=1, 
+#     patient=2, 
+#     checkpoint_from_epoch=1, 
+#     use_checkpoint=None,
+#     warm_up = 3
+# )
 
 pipe = FilterBandTFPipeline(
     model = model, 
--- I_Wanna_Hear_Your_Voice/network/models/__init__.py
+++ I_Wanna_Hear_Your_Voice/network/models/__init__.py
@@ -1,2 +1,3 @@
 from .TF_gridnet import TF_Gridnet
 from .TF_gridnet_with_condition import *
+from .tfGrideNetLOTH import *
