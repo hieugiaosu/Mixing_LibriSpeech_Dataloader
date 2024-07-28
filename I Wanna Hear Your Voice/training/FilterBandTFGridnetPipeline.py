4ef73de86dcf3674f718bf577005d9003e64027a
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-07-28 14:15:16 +0700

    adjust import error

--- I Wanna Hear Your Voice/training/FilterBandTFGridnetPipeline.py
+++ I Wanna Hear Your Voice/training/FilterBandTFGridnetPipeline.py
@@ -1,6 +1,7 @@
 from torch.nn.modules import Module
 from .utils import TrainingPipeline
-from ..criterion import SI_SDR_Loss, Mixture_constraint_loss
+from ..criterion.si_sdr import SI_SDR_Loss
+from ..criterion.mixture_constraint_loss import Mixture_constraint_loss
 from torch.cuda.amp import GradScaler, autocast
 import torch
 import time
