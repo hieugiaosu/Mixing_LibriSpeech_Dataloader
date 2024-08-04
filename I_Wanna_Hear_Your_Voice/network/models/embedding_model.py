4e62e58f2fe5fcb6df248caf3a9d72665379d40f
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-08-04 20:57:10 +0700

    fix bug in embedding

--- I_Wanna_Hear_Your_Voice/network/models/embedding_model.py
+++ I_Wanna_Hear_Your_Voice/network/models/embedding_model.py
@@ -2,13 +2,14 @@ import torch
 from resemblyzer import VoiceEncoder
 
 class ResemblyzerVoiceEncoder:
-    def __init__(self,device) -> None:
+    def __init__(self, device) -> None:
         self.model = VoiceEncoder(device)
+        
     def __call__(self, audio: torch.Tensor):
-        if audio.size() == 1:
+        if audio.ndimension() == 1:
             return torch.tensor(self.model.embed_utterance(audio.numpy())).float().cpu()
         else:
+            print(audio.shape)
             e = torch.stack([torch.tensor(self.model.embed_utterance(audio[i,:].numpy())).float().cpu() 
-                            for i in range(audio.shape[0])]
-                            )
+                             for i in range(audio.shape[0])])
             return e
