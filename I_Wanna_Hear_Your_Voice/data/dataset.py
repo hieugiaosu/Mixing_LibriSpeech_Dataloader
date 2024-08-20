d4312d01ec574b3621c05f0ee77b6a4134ad3531
Tanio253 <thanh.tran159@hcmut.edu.vn>
2024-08-20 22:31:13 +0700

    debug

--- I_Wanna_Hear_Your_Voice/data/dataset.py
+++ I_Wanna_Hear_Your_Voice/data/dataset.py
@@ -163,12 +163,12 @@ class Wsj02MixDataset(Dataset):
             gain = np.max([1., np.max(np.abs(mix_np)), np.max(np.abs(sources_np))]) / 0.9
             mix_np_max = mix_np / gain
             sources_np_max = sources_np / gain
-            print("length")
-            print(len(mix_np_max))
-            print(len(sources_np_max[0]))
-            print(len(sources_np_max[1]))
-            print(e.shape)
-            print("length 1")
+            # print("length")
+            # print(len(mix_np_max))
+            # print(len(sources_np_max[0]))
+            # print(len(sources_np_max[1]))
+            # print(e.shape)
+            # print("length 1")
             return {"mix": mix_np_max, "src0": sources_np_max[0], "src1": sources_np_max[1], "emb0": e}
 
         if self.mode == "min":
